e08a26f71de221eec0a0adbc07b647e21d1386c5
Charles Chen <chen.charles.c@gmail.com>
2021-11-27 18:22:47 -0500

    Added Google Cloud Run

--- api/Program.cs
+++ api/Program.cs
@@ -1,13 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// In the container environment, we want to run this on 8080 for GCR
+// This means that we will need to add the environment variable in the
+// Dockerfile.
+if(app.Environment.IsEnvironment("container")) {
+    app.Urls.Add("http://0.0.0.0:8080"); // Supoorts Google Cloud Run.
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,8 +22,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
